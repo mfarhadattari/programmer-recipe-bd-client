72ceea5ae72c64213e89cd810b8df1008077c3ac
mfarhadattari <mfarhad.attari@gmail.com>
2023-05-05 11:32:26 +0600

    set footer bottom using flex

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -7,9 +7,11 @@ import Loader from "../components/Loader/Loader";
 const Main = () => {
   const navigation = useNavigation();
   return (
-    <div className="font-natos">
-      <Header></Header>
-      {navigation.state === "loading" ? <Loader></Loader> : <Outlet></Outlet>}
+    <div className="font-natos min-h-screen flex flex-col justify-between">
+      <div>
+        <Header></Header>
+        {navigation.state === "loading" ? <Loader></Loader> : <Outlet></Outlet>}
+      </div>
       <Footer></Footer>
     </div>
   );
